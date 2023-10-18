d2b38f3339085f4ed31172a260ed95dff1e6dd0f
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-19 00:50:31 +0800

    Added Modules and API Endpoint

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+export default defineNuxtConfig({
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  runtimeConfig: {
+    public: {
+      appUrl: process.env.BASE_URL,
+    },
+  },
+  modules: ["@pinia/nuxt"],
+  css: ["~/assets/css/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      "defineStore", // import { defineStore } from 'pinia'
+      ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+  devtools: { enabled: true },
+});
---
+++ store/task.ts
@@ -0,0 +1,99 @@
+export const useTaskStore = defineStore("task", {
+  state: () => ({
+    task: null as TaskInfo | null,
+    user: null as UserInfo | null,
+  }),
+  actions: {
+    async fetchTasks() {
+      try {
+        const config = useRuntimeConfig();
+        const response = await $fetch(`${config.public.appUrl}/tasks`, {
+          method: "GET",
+        });
+        this.task = response;
+      } catch (error) {
+        console.error("Error on Fetching Tasks: ", error);
+      }
+    },
+    async createNewTask(data: TaskInfo) {
+      try {
+        const config = useRuntimeConfig();
+        const response = await $fetch(`${config.public.appUrl}/tasks`, {
+          method: "POST",
+          body: {
+            ...data,
+          },
+        });
+        this.task.push(response);
+      } catch (error) {
+        console.error("Error on Creating New Task: ", error);
+      }
+    },
+    async updateExistingTask(id: string, data: TaskInfo) {
+      try {
+        const config = useRuntimeConfig();
+        const response = await $fetch(`${config.public.appUrl}/tasks/${id}`, {
+          method: "PUT",
+          body: {
+            ...data,
+          },
+        });
+        this.task.map((x) => {
+          x.id === response.id ? { ...x, ...reponse } : x;
+        });
+      } catch (error) {
+        console.error("Error on Updating Exiting Task: ", error);
+      }
+    },
+    async deleteTask(id: string) {
+      try {
+        const config = useRuntimeConfig();
+        const response = await $fetch(`${config.public.appUrl}/tasks/${id}`, {
+          method: "DELETE",
+        });
+        this.task = this.task.filter((x) => x.id !== id);
+      } catch (error) {
+        console.error("Error on Deleting Task: ", error);
+      }
+    },
+    async searchTask(query: string) {
+      const config = useRuntimeConfig();
+      const response = await $fetch(`${config.public.appUrl}/tasks`, {
+        method: "GET",
+        params: {
+          search: query,
+          sortBy: "title",
+          order: "desc",
+        },
+      });
+      this.task = response;
+    },
+    async fetchUsers() {
+      try {
+        const config = useRuntimeConfig();
+        const response = await $fetch(`${config.public.appUrl}/users`, {
+          method: "GET",
+        });
+        this.user = response;
+      } catch (error) {
+        console.error("Error on Fetching Users: ", error);
+      }
+    },
+  },
+});
+
+interface TaskInfo {
+  title: string;
+  created_at: Date;
+  assignee: UserInfo;
+  is_done: boolean;
+  is_important: boolean;
+  sort: number;
+  id: string;
+}
+
+interface UserInfo {
+  name: string;
+  avatar: string;
+  id: string;
+}
