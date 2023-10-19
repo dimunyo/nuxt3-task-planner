c649bb63f24799b9847def36b5e93881cb2b6318
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-19 19:04:18 +0800

    remove the boilerplate

--- app.vue
+++ app.vue
@@ -1,26 +1,7 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-<script setup>
-import { useTaskStore } from '~/store/task'
-const store = useTaskStore();
-
-const formData = ref({
-  title: "SAMPLE STRINGYYZ",
-  assignee: {},
-  is_done: false,
-  is_important: false,
-  sort: 2,
-})
-onMounted(async () => {
-  //await store.createNewTask(formData.value);
-  //await store.updateExistingTask("100", formData.value);
-  //await store.fetchUsers();
-  //await store.deleteTask("102");
-  await store.searchTask("SAMPLE STRING")
-})
-</script>
