209cb479a50e66d5cea94938cee99d38a3451241
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-21 19:43:03 +0800

    added custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-custom-color min-h-screen">
+        <NavBar class="sticky top-0" />
+        <div class="flex gap-4 relative mx-auto py-6 lg:flex w-11/12">
+            <SideBar class="fixed left-10 flex-wrap" />
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.bg-custom-color {
+    background-color: #F6F9FD;
+}
+</style>
