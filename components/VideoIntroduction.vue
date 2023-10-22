7d464e36aeb825a64799f3f1281bffb2b92329b6
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 13:35:48 +0800

    Addd Video Introduction component

---
+++ components/VideoIntroduction.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-white shadow-md">
+        <h1 class="text-md inline-block font-medium py-2 px-5">Video Introduction</h1>
+        <iframe class="aspect-square w-full" src="https://www.youtube.com/embed/5LrG333wKy8" title="Video Script"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>
+</template>
