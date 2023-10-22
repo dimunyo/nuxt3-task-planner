faaf6809bfc5f5445769f615fc261852d6aa5164
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 13:34:47 +0800

    Added Profile Progress Components

---
+++ components/ProfileProgress.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-white shadow-md px-5">
+        <div class="flex w-full  py-2  items-end justify-between">
+            <p class="text-md inline-block font-medium">Profile Completion</p>
+            <span class="text-xs text-green-400">100%</span>
+        </div>
+        <div class="w-full bg-gray-200 rounded-full h-1.5">
+            <div class="bg-green-400 h-1.5 rounded-full w-full"></div>
+        </div>
+        <div class="flex flex-col w-full justify-center items-center my-5">
+            <h1 class="text-sm text-green-500">Well done, Reyven!</h1>
+            <img src="~/assets/images/award.png" />
+            <p class="text-xs pt-5 text-gray-500">You have one of the strongest professional profile in our
+                talent pool.
+                Expect
+                progress on your
+                application at anytime soon.</p>
+        </div>
+    </div>
+</template>
