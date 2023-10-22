22705fa161dbe94f9782463e750bcc3228d73bf0
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 13:34:35 +0800

    Added Professional Status Components

---
+++ components/ProfessionalStatus.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bg-white w-full relative border-b border-gray-300 p-6">
+        <div class="absolute right-5 top-7">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-5 h-5 text-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+            </svg>
+        </div>
+        <h1 class="text-lg font-medium">Professional Status</h1>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Recent Job Title</p>
+            <p class="text-sm text-gray-700">Web Designer and Developer</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Interview Availability</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Current Month Salary (gross)</p>
+            <p class="text-sm text-green-400">Php 30,000</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Allowance (Non-Taxable)</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Pre-employement Leaves</p>
+            <p class="text-sm text-gray-700">07 July - Birthday</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Where Can you Work?</p>
+            <p class="text-sm text-gray-700">Office and Home</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Estimated Travel to Office</p>
+            <p class="text-sm text-gray-700">15 mins</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Connection Type</p>
+            <p class="text-sm text-gray-700">Globe Broadband</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Website Portfolio</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+    </div>
+</template>
