14a8f6da2b15f0e206ceda8535cf0243e3a6e3c8
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 14:19:55 +0800

    Added Cursor Pointer

--- components/ProfessionalStatus.vue
+++ components/ProfessionalStatus.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white w-full relative border-b border-gray-300 p-6">
-        <div class="absolute right-5 top-7">
+        <div class="absolute right-5 top-7 cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                 class="w-5 h-5 text-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round"
