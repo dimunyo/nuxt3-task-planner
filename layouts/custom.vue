bec8fa64a697fdab9aba9d421bf0c3fc28b0e3e3
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 13:36:05 +0800

    Change layout to be 8/12

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-custom-color min-h-screen">
         <NavBar class="sticky top-0" />
-        <div class="flex gap-4 relative mx-auto py-6 lg:flex w-11/12">
-            <SideBar class="fixed left-10 flex-wrap" />
+        <div class="flex gap-4 relative mx-auto pl-52 py-6 lg:flex w-8/12">
+            <SideBar class="fixed left-64 flex-wrap" />
             <slot />
         </div>
     </div>
