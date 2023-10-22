ac589d6a04bc071b45d3bdea0433622a806bcbe6
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 13:32:57 +0800

    Added General Details Component

---
+++ components/GeneralDetails.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bg-white w-full relative border-b border-gray-300 p-6">
+        <div class="absolute right-5 top-7">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-5 h-5 text-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+            </svg>
+        </div>
+        <h1 class="text-lg font-medium">General Details</h1>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Date of Birth</p>
+            <p class="text-sm text-gray-700">07 July 1986</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Gender</p>
+            <p class="text-sm text-gray-700">Male</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Civil Status</p>
+            <p class="text-sm text-gray-700">Married</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Nationality</p>
+            <p class="text-sm text-gray-700">Filipino</p>
+        </div>
+    </div>
+</template>
