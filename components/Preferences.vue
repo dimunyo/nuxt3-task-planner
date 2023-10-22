1e34d8d9dae7f93df3b89f76d3af96e0a0317877
dimunyo <55717823+dimunyo@users.noreply.github.com>
2023-10-22 13:34:24 +0800

    Added preference component

---
+++ components/Preferences.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="bg-white w-full relative border-b border-gray-300 p-6">
+        <div class="absolute right-5 top-7">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-5 h-5 text-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+            </svg>
+        </div>
+        <h1 class="text-lg font-medium">Preferences</h1>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Day Shift</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Mid Shift</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Night Shift</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">English Only Policy (EOP)</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Where did you here us?</p>
+            <p class="text-sm text-gray-700">-</p>
+
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Marketing Permission</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+        <div class="flex flex-wrap pt-5">
+            <p class="detail text-gray-400 text-sm w-5/12">Refferal</p>
+            <p class="text-sm text-gray-700">-</p>
+        </div>
+    </div>
+</template>
